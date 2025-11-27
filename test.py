3b9f1e1dc4ab594eb2e3a271cbe23c00ed6416da
Jean Pierre <SrBaguette>
2025-11-27 08:47:27 -0500

    Se añadió al test una linea para probar el print en dado caso de que se ponga cierto atributo

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ from admindatos import AdminDatos
 
 lista=AdminDatos.leer_csv("dataset3_clinical_supplies_inventory.csv")
 AdminDatos.mostrar(lista)
+
+print("Ejemplo de stock por warehouse")
+print(AdminDatos.stockpor(lista,"warehouse_area"))
